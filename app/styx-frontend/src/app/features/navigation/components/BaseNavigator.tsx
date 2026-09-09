e30035dbb3b9f938195878c20dbb2fb02273a3a2
Maija Lintunokka <maija.lintunokka@nosto.com>
2026-09-09 18:22:46 +0300

    fix: hide global search

--- app/styx-frontend/src/app/features/navigation/components/BaseNavigator.tsx
+++ app/styx-frontend/src/app/features/navigation/components/BaseNavigator.tsx
@@ -17,7 +17,6 @@ import { useCheckRouteMatch } from '@/router-utils/hooks/useCheckRouteMatch'
 import { AnnouncementView } from '../../announcements/AnnouncementView'
 import { getAuthState } from '../../auth/AuthSliceSelectors'
 import { SmallProfile } from '../../auth/smallProfile/SmallProfile'
-import { GlobalSearch } from '../../globalSearch/GlobalSearch'
 import { ThemeModeToggle } from '../../theming/components/ThemeModeToggle'
 import { CustomTheme, useCustomTheme } from '../../theming/hooks/useCustomTheme'
 import { DocsNavigatorButton } from './DocsNavigatorButton'
@@ -102,7 +101,6 @@ export const BaseNavigator = () => {
 				{!user && <LandingPageNavigatorButton icon={<TenebrieLogoInline />} label="Neverkin" />}
 			</Box>
 			<Stack direction="row" gap={2} alignItems="center" height={1}>
-				<GlobalSearch />
 				<ThemeModeToggle />
 				{user && !isNarrow && <AnnouncementView />}
 				<Divider orientation="vertical" sx={{ height: '20px' }} />
