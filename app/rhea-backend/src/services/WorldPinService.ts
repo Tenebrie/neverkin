77c6c8ba5a55eee7b585a850ed0619deffe979c8
Maija Lintunokka <maija.lintunokka@nosto.com>
2026-09-10 17:01:31 +0300

    fix: avoid touching world on pin change

--- app/rhea-backend/src/services/WorldPinService.ts
+++ app/rhea-backend/src/services/WorldPinService.ts
@@ -1,7 +1,5 @@
 import { getPrismaClient } from '@src/services/dbClients/DatabaseClient.js'
 
-import { makeTouchWorldQuery } from './dbQueries/makeTouchWorldQuery.js'
-
 export const WorldPinService = {
 	toggleWorldPin: async (params: { worldId: string; userId: string }) => {
 		return await getPrismaClient().$transaction(async (dbClient) => {
@@ -17,7 +15,6 @@ export const WorldPinService = {
 					where: { userId: params.userId, rank: { gt: existingPin.rank } },
 					data: { rank: { decrement: 2 } },
 				})
-				await makeTouchWorldQuery(params.worldId, dbClient)
 				return {
 					pinned: false,
 					rank: 0,
@@ -30,7 +27,6 @@ export const WorldPinService = {
 			await dbClient.userWorldPin.create({
 				data: { ...params, rank: pinCount * 2 },
 			})
-			await makeTouchWorldQuery(params.worldId, dbClient)
 			return {
 				pinned: true,
 				rank: pinCount * 2,
